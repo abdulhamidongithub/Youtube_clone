4987519146474121e1c7b4fb9ed86c9968d5d8fb
abdulhamidongithub <1997abdulhamid@gmail.com>
2021-12-23 17:42:35 +0300

    Modelsni boshladim

--- channels/models.py
+++ channels/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=20)
+    joined_date = models.DateField(auto_now_add=True)
+    photo = models.URLField(blank=True)
+
